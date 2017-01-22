9fc86eb25254dece851e381399d0f72c74cdf8bc
winfredselwyn <winfredselwyn@gmail.com>
2017-01-22 13:36:19 +0000

    initial commit
    Conway's Game of Life, implementeented with Python. Compulsory libraries are numpy and matplotlib

--- game_of_life.py
+++ game_of_life.py
@@ -1,1 +1,16 @@
-print "hello world"
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ON = 255
+OFF = 0
+vals = [ON, OFF]
+
+def randomGrid(N):
+	return np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
+	
+def addGlider(i, j, grid):
+	glider = np.array([[0  ,   0, 255],
+					   [255,   0, 255],
+					   [0  , 255, 255]])
+	grid[]
